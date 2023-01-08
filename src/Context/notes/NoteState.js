0a8c755c4e680ed8571a3c702b2b33027015df20
Jayesh504 <pjayesh504@gmail.com>
2023-01-08 22:47:22 +0530

    Video 63 completed

--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -1,80 +1,107 @@
 import NoteContext from "./noteContext";
 import { useState } from "react";
 
-const NoteState = (props)=>{
-    const notesInitial = [
-        {
-          "_id": "61322f19553781a8ca8d0e06",
-          "user": "6131dc5e3e4037cd4734a066",
-          "title": "My Title",
-          "description": "Please wake up early",
-          "tag": "personal",
-          "date": "2021-09-03T14:20:09.509Z",
-          "__v": 0
-        },
-        {
-          "_id": "61322f19553781a8ca8d0e08",
-          "user": "6131dc5e3e4037cd4734a066",
-          "title": "My Title",
-          "description": "Please wake up early",
-          "tag": "personal",
-          "date": "2021-09-03T14:20:09.668Z",
-          "__v": 0
-        },
-        {
-          "_id": "61322f19553781a8ca8d0e081",
-          "user": "6131dc5e3e4037cd4734a066",
-          "title": "My Title",
-          "description": "Please wake up early",
-          "tag": "personal",
-          "date": "2021-09-03T14:20:09.668Z",
-          "__v": 0
-        },
-        {
-          "_id": "61322f19553781a8ca8d0e0811",
-          "user": "6131dc5e3e4037cd4734a066",
-          "title": "My Title",
-          "description": "Please wake up early",
-          "tag": "personal",
-          "date": "2021-09-03T14:20:09.668Z",
-          "__v": 0
-        },
-        {
-          "_id": "61322f19553781a8ca8d0e08111",
-          "user": "6131dc5e3e4037cd4734a066",
-          "title": "My Title",
-          "description": "Please wake up early",
-          "tag": "personal",
-          "date": "2021-09-03T14:20:09.668Z",
-          "__v": 0
-        },
-        {
-          "_id": "61322f19553781a8ca8d0e082",
-          "user": "6131dc5e3e4037cd4734a066",
-          "title": "My Title",
-          "description": "Please wake up early",
-          "tag": "personal",
-          "date": "2021-09-03T14:20:09.668Z",
-          "__v": 0
-        },
-        {
-          "_id": "61322f19553781a8ca8d0e0822",
-          "user": "6131dc5e3e4037cd4734a066",
-          "title": "My Title",
-          "description": "Please wake up early",
-          "tag": "personal",
-          "date": "2021-09-03T14:20:09.668Z",
-          "__v": 0
-        },
-      ]
-      const [notes, setNotes] = useState(notesInitial)
+const NoteState = (props) => {
+  const notesInitial = [
+    {
+      "_id": "61322f19553781a8ca8d0e06",
+      "user": "6131dc5e3e4037cd4734a066",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2021-09-03T14:20:09.509Z",
+      "__v": 0
+    },
+    {
+      "_id": "61322f19553781a8ca8d0e08",
+      "user": "6131dc5e3e4037cd4734a066",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2021-09-03T14:20:09.668Z",
+      "__v": 0
+    },
+    {
+      "_id": "61322f19553781a8ca8d0e081",
+      "user": "6131dc5e3e4037cd4734a066",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2021-09-03T14:20:09.668Z",
+      "__v": 0
+    },
+    {
+      "_id": "61322f19553781a8ca8d0e0811",
+      "user": "6131dc5e3e4037cd4734a066",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2021-09-03T14:20:09.668Z",
+      "__v": 0
+    },
+    {
+      "_id": "61322f19553781a8ca8d0e08111",
+      "user": "6131dc5e3e4037cd4734a066",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2021-09-03T14:20:09.668Z",
+      "__v": 0
+    },
+    {
+      "_id": "61322f19553781a8ca8d0e082",
+      "user": "6131dc5e3e4037cd4734a066",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2021-09-03T14:20:09.668Z",
+      "__v": 0
+    },
+    {
+      "_id": "61322f19553781a8ca8d0e0822",
+      "user": "6131dc5e3e4037cd4734a066",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2021-09-03T14:20:09.668Z",
+      "__v": 0
+    },
+  ]
+  const [notes, setNotes] = useState(notesInitial)
 
-    
-    return (
-        <NoteContext.Provider value={{notes, setNotes}}>
-            {props.children}
-        </NoteContext.Provider>
-    )
+
+  // Add a Note
+  const addNote = (title, description, tag) => {
+    // TODO: API Call
+    console.log("Adding a new note")
+    const note = {
+      "_id": "61322f19553781a8ca8d0e06",
+      "user": "6131dc5e3e4037cd4734a066",
+      "title": title,
+      "description": description,
+      "tag": tag,
+      "date": "2021-09-03T14:20:09.509Z",
+      "__v": 0
+    };
+    setNotes(notes.concat(note))
+  }
+
+  // Delete a Note
+  const deletNote = () => {
+
+  }
+
+  // Edit a Note
+  const editNote = () => {
+
+  }
+
+
+  return (
+    <NoteContext.Provider value={{ notes, addNote, deletNote, editNote }}>
+      {props.children}
+    </NoteContext.Provider>
+  )
 }
 
 export default NoteState;
